d43978def785eb1c08f4c7acf9aef81b0c13b928
anitha1506 <99156177+anitha1506@users.noreply.github.com>
2022-02-14 14:26:37 -0500

    Add files via upload

--- TC_LoginTest_Banking.java
+++ TC_LoginTest_Banking.java
@@ -1,5 +1,40 @@
 package TestBanking;
 
-public class TC_LoginTest_Banking {
+import java.io.IOException;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class TC_LoginTest_Banking extends BaseClass
+{
+
+	@Test
+	public void loginTest() throws IOException 
+	{
+			
+		logger.info("URL is opened");
+		
+		LoginPage lp=new LoginPage(driver);
+		lp.setUserName(username);
+		logger.info("Entered username");
+		
+		lp.setPassword(password);
+		logger.info("Entered password");
+		
+		lp.clickSubmit();
+		
+		if(driver.getTitle().equals("Guru99 Bank Manager HomePage"))
+		{
+			Assert.assertTrue(true);
+			logger.info("Login test passed");
+		}
+		else
+		{
+			captureScreen(driver,"loginTest");
+			Assert.assertTrue(false);
+			logger.info("Login test failed");
+		}
+		
+	}
 }
